733150822991d73894c4bdcac6ff4d7ca06c9aec
Zbigniew Gajewski <z_g@op.pl>
2017-09-25 17:01:56 +0200

    C# Consumer of the libfar (FsComtrade.AppCs) added.

---
+++ src/FsComtrade.AppCs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FsComtrade.Lib.Types;
+using FsComtrade.Lib.Mappers;
+
+namespace FsComtrade.AppCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = TypesModule.FilePath.NewFullFilePath("..\\FsComtrade\\src\\Resources\\TestFiles\\Test.cfg");
+            var cfgFileLines = CfgModule.getCfgFileLines(filePath);
+            var cfgFile = CfgModule.mapCfgFile(cfgFileLines);
+                        
+            Console.WriteLine(cfgFile.StationName);
+            Console.WriteLine(cfgFile.RecordingDeviceId);
+            Console.WriteLine(cfgFile.RevisionYear);
+            Console.WriteLine(cfgFile.TotalNumberOfChannels);
+            Console.WriteLine(cfgFile.AnalogNumberOfChannels);
+            Console.WriteLine(cfgFile.DigitalNumberOfChannels);
+            Console.WriteLine(cfgFile.NominalFrequencyHz);
+            Console.WriteLine(cfgFile.FirstSampleTimeStamp);
+            Console.WriteLine(cfgFile.TriggerPointTimeStamp);
+            Console.WriteLine(cfgFile.FileType);
+            Console.WriteLine(cfgFile.MultiplicationFactor);
+        }
+    }
+}
