b774b220074e3c482d2ab7d554b3d5e872dc89e9
Zbigniew Gajewski <z_g@op.pl>
2017-09-25 16:11:38 +0200

    F# Consumer of the library (FsComtrade.AppFs) added.

---
+++ src/FsComtrade.AppFs/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+open FsComtrade.Lib
+open FsComtrade.Lib.Types.TypesModule
+open FsComtrade.Lib.Mappers.CfgModule
+
+[<EntryPoint>]
+let main argv =
+    let cfgFile = 
+        (FullFilePath "..\FsComtrade\src\Resources\TestFiles\Test.cfg")
+        |> getCfgFileLines
+        |> mapCfgFile
+      
+    printfn "%s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n" 
+        cfgFile.StationName 
+        cfgFile.RecordingDeviceId 
+        (cfgFile.RevisionYear.ToString())
+        (cfgFile.TotalNumberOfChannels.ToString()) 
+        (cfgFile.AnalogNumberOfChannels.ToString()) 
+        (cfgFile.DigitalNumberOfChannels.ToString())
+        (cfgFile.NominalFrequencyHz.ToString())
+        (cfgFile.FirstSampleTimeStamp.ToString())
+        (cfgFile.TriggerPointTimeStamp.ToString())
+        (cfgFile.FileType.ToString())
+        (cfgFile.MultiplicationFactor.ToString())
+    0 // return an integer exit code
